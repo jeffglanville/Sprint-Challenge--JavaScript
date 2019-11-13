35f88a839c852859a14240566317239f7595b78b
Jeff Glanville <jeff.glanville@outlook.com>
2019-11-13 16:56:42 -0600

    update multiple files

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -134,12 +134,18 @@ function animals(zooAnimals){
 
 /* Request 2: .map()
 
-The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
+The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the result.
 
 */
 
-const lowCaseAnimalNames = [];
-console.log(lowCaseAnimalNames);
+function animalNamesAllLowerCase(zooAnimals){
+  const lowCaseAnimalNames = zooAnimals.map((animal) =>
+  animal.animal_name.toLowerCase());
+
+  return lowCaseAnimalNames;
+}
+animalNamesAllLowerCase(zooAnimals);
+
 
 /* Request 3: .filter()
 
@@ -157,8 +163,10 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
-console.log(populationTotal);
+const animalPop = zooAnimals.reduce(function(accumulator, currentValue){
+  return accumulator + currentValue.population
+}, 0);
+console.log(animalPop);
 
 
 /*
